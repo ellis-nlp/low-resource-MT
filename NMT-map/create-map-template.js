4a8789c11f1ebd81f824f0ff34f73d11e5ac27fe
Joerg Tiedemann <jorg.tiedemann@helsinki.fi>
2022-01-24 17:53:43 +0200

    removed wmflabs

--- NMT-map/create-map-template.js
+++ NMT-map/create-map-template.js
@@ -3,7 +3,9 @@ var markers = [],
     flayers = {},
     map = L.map('map', {fullscreenControl: true}).setView([35, 160], 2);
 
-var OpenStreetMap_BlackAndWhite = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+// OLD: does not work anymore:
+// var OpenStreetMap_BlackAndWhite = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+var OpenStreetMap_BlackAndWhite = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 18,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 });
